5e55765d80921de9cb7b81278a841eecd1cf0b90
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-20 00:47:04 +0100

    chore: Update landing page colors and styles

--- components/features.tsx
+++ components/features.tsx
@@ -51,7 +51,7 @@ const features = [
 
 export default function Features() {
   return (
-    <div className='bg-gray-900 py-24 sm:py-32'>
+    <div id="features" className='bg-gray-900 py-24 sm:py-32'>
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         <div className='mx-auto max-w-2xl sm:text-center'>
           <h2 className='text-base font-semibold leading-7 text-bright-yellow'>
