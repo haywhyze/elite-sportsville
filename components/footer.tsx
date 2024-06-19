301f2a897e5c98ed343a13cb1eb9873deb18acd3
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-19 15:06:02 +0100

    Finish with design

--- components/footer.tsx
+++ components/footer.tsx
@@ -62,14 +62,14 @@ const navigation = [
 
 export default function Footer() {
   return (
-    <footer className='bg-white'>
-      <div className='mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8'>
+    <footer className='bg-gray-900' aria-labelledby='footer-heading'>
+      <div className='border-t border-white/10 mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8'>
         <div className='flex justify-center space-x-6 md:order-2'>
           {navigation.map((item) => (
             <a
               key={item.name}
               href={item.href}
-              className='text-gray-400 hover:text-gray-500'
+              className='text-gray-500 hover:text-gray-400'
             >
               <span className='sr-only'>{item.name}</span>
               <item.icon className='h-6 w-6' aria-hidden='true' />
@@ -77,7 +77,7 @@ export default function Footer() {
           ))}
         </div>
         <div className='mt-8 md:order-1 md:mt-0'>
-          <p className='text-center text-xs leading-5 text-gray-500'>
+          <p className='mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0'>
             &copy; 2024 Elite Sportsville Ltd, Inc. All rights reserved.
           </p>
         </div>
