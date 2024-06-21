f4d143d6fa25eb046a9abd5e1644f9768aea7344
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-21 13:47:15 +0100

    refactor: Update booking modal to improve user experience

--- components/compositions/common/date-picker.tsx
+++ components/compositions/common/date-picker.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import { addMonths, format } from 'date-fns';
+import { forwardRef } from 'react';
 import DatePicker from 'react-datepicker';
+import { setDefaultLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import './date-picker.css';
 
 interface CustomDatePickerProps {
   selectedDate: Date | null;
   handleDateChange: (date: Date | null) => void;
 }
 
+setDefaultLocale('en-GB');
+
 const CustomDatePicker: React.FC<CustomDatePickerProps> = ({
   selectedDate,
   handleDateChange,
 }) => {
+  const CustomInput = forwardRef<HTMLButtonElement, any>(
+    ({ value, onClick }, ref) => (
+      <button
+        onClick={() => onClick()}
+        type='button'
+        ref={ref}
+        className='rounded-md bg-bright-purple px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-bright-purple-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bright-purple'
+      >
+        {value}
+      </button>
+    )
+  ) as React.ForwardRefRenderFunction<HTMLButtonElement, any>;
+
+  CustomInput.displayName = 'CustomInput';
+
   return (
     <div>
-      <DatePicker selected={selectedDate} onChange={handleDateChange} />
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleDateChange}
+        minDate={new Date()}
+        maxDate={addMonths(new Date(), 3)}
+        dateFormat='eeee, do MMMM, yyyy'
+        placeholderText='Select a date between today and 3 months from now'
+        customInput={<CustomInput />}
+        className='block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-bright-purple focus:border-bright-purple sm:text-sm'
+        calendarClassName='custom-calendar'
+      />
     </div>
   );
 };
