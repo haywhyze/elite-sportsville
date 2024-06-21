7f003b1038d43d2d40e523e17025a61e90d7c93e
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-21 07:30:26 +0100

    chore: Update landing page with booking modal functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useState } from 'react';
 import Footer from '@/compositions/footer';
 import Contact from '@/compositions/contact';
 import Features from '@/compositions/features';
@@ -11,14 +14,15 @@ import Testimonials from '@/compositions/testimonials';
 // export const dynamic = 'force-dynamic';
 
 export default function Home() {
+  const [bookingModalOpen, setBookingModalOpen] = useState(false);
   return (
     <main className='relative flex min-h-screen flex-col'>
       {/* <Header /> */}
-      <Hero />
+      <Hero setBookingModalOpen={setBookingModalOpen} />
       <Features />
       <Pricing />
       <Testimonials />
-      <Contact /> 
+      <Contact />
       <Footer />
     </main>
   );
