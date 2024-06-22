1b6c81c733c8d030048911d9087b9d85acb50a44
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-22 18:54:36 +0100

    refactor: Add toast notifications for booking errors

--- components/compositions/common/time-slot.tsx
+++ components/compositions/common/time-slot.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+
 const TimeSlot = ({
   time,
   price,
@@ -13,13 +15,16 @@ const TimeSlot = ({
 }) => {
   return (
     <button
-      className={`px-1 py-2 w-[calc(35%-1rem)] lg:w-[calc(20%-1rem)] text-xs lg:text-sm border ${
-        selected ? 'bg-dark-purple text-white' : 'text-dark-purple bg-green-50'
-      } rounded-md shadow-sm ${
-        isBooked
-          ? 'cursor-not-allowed bg-gray-600 text-gray-100'
-          : 'cursor-pointer lg:hover:bg-dark-purple lg:hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bright-yellow'
-      }`}
+      className={classNames(
+        'px-1 py-2 w-[calc(35%-1rem)] lg:w-[calc(20%-1rem)] text-xs lg:text-sm border rounded-md shadow-sm',
+        {
+          'bg-dark-purple text-white': selected,
+          'text-dark-purple bg-green-50': !selected && !isBooked,
+          'cursor-not-allowed bg-gray-600 text-gray-100': isBooked,
+          'cursor-pointer lg:hover:bg-dark-purple lg:hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bright-yellow':
+            !isBooked,
+        }
+      )}
       disabled={isBooked}
       onClick={onSelect}
       type='button'
