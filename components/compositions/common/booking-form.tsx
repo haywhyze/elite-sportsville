5cfcd5a6e4f17158a85e52ca148edd4e58a6adfb
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-26 22:44:21 +0100

    refactor: Update booking form component with selected time slots and date

--- components/compositions/common/booking-form.tsx
+++ components/compositions/common/booking-form.tsx
@@ -1,121 +1,50 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-export default function BookingForm() {
-  return (
-    <form
-      action='#'
-      method='POST'
-      className='px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48'
-    >
-      <div className='mx-auto max-w-xl lg:mr-0 lg:max-w-lg'>
-        <div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
-          <div>
-            <label
-              htmlFor='first-name'
-              className='block text-sm font-semibold leading-6 text-white'
-            >
-              First name
-            </label>
-            <div className='mt-2.5'>
-              <input
-                type='text'
-                name='first-name'
-                id='first-name'
-                autoComplete='given-name'
-                className='block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-bright-purple-dark sm:text-sm sm:leading-6'
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              htmlFor='last-name'
-              className='block text-sm font-semibold leading-6 text-white'
-            >
-              Last name
-            </label>
-            <div className='mt-2.5'>
-              <input
-                type='text'
-                name='last-name'
-                id='last-name'
-                autoComplete='family-name'
-                className='block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-bright-purple-dark sm:text-sm sm:leading-6'
-              />
-            </div>
-          </div>
-          <div className='sm:col-span-2'>
-            <label
-              htmlFor='email'
-              className='block text-sm font-semibold leading-6 text-white'
-            >
-              Email
-            </label>
-            <div className='mt-2.5'>
-              <input
-                type='email'
-                name='email'
-                id='email'
-                autoComplete='email'
-                className='block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-bright-purple-dark sm:text-sm sm:leading-6'
-              />
-            </div>
-          </div>
-          <div className='sm:col-span-2'>
-            <label
-              htmlFor='phone-number'
-              className='block text-sm font-semibold leading-6 text-white'
-            >
-              Phone number
-            </label>
-            <div className='mt-2.5'>
-              <input
-                type='tel'
-                name='phone-number'
-                id='phone-number'
-                autoComplete='tel'
-                className='block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-bright-purple-dark sm:text-sm sm:leading-6'
-              />
-            </div>
-          </div>
-          <div className='sm:col-span-2'>
-            <label
-              htmlFor='message'
-              className='block text-sm font-semibold leading-6 text-white'
-            >
-              Message
-            </label>
-            <div className='mt-2.5'>
-              <textarea
-                name='message'
-                id='message'
-                rows={4}
-                className='block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-bright-purple-dark sm:text-sm sm:leading-6'
-                defaultValue={''}
-              />
-            </div>
-          </div>
-        </div>
-        <div className='mt-8 flex justify-end'>
-          <button
-            type='submit'
-            className='rounded-md bg-bright-purple-dark px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-bright-purple focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bright-purple-dark'
-          >
-            Send message
-          </button>
-        </div>
-      </div>
-    </form>
-  );
+import React from 'react';
+import { Input } from '@/components/input';
+import { Textarea } from '@/components/textarea';
+import { Fieldset, Label } from '@/components/fieldset';
+import SelectedTimeSlotsList from './selected-time-slots-list';
+
+interface BookingFormProps {
+  selectedTimeSlots: string[];
+  selectedDate: Date | null;
 }
+
+const BookingForm: React.FC<BookingFormProps> = ({
+  selectedTimeSlots,
+  selectedDate,
+}) => {
+  return (
+    <>
+      {selectedDate && (
+        <SelectedTimeSlotsList
+          selectedTimeSlots={selectedTimeSlots}
+          // setSelectedTimeSlots={setSelectedTimeSlots}
+        />
+      )}
+      <form className='mt-6 w-full'>
+        {/* <div className='mt-6'>
+        <h4 className='text-md font-medium'>Pricing:</h4>
+        <p className='mt-2'>Total: ₦</p>
+      </div> */}
+        <Fieldset className='mb-4'>
+          <Label htmlFor='name'>Name</Label>
+          <Input type='text' name='name' id='name' required />
+        </Fieldset>
+        <Fieldset className='mb-4'>
+          <Label htmlFor='email'>Email</Label>
+          <Input type='email' name='email' id='email' required />
+        </Fieldset>
+        <Fieldset className='mb-4'>
+          <Label htmlFor='phone'>Phone</Label>
+          <Input type='tel' name='phone' id='phone' required />
+        </Fieldset>
+        <Fieldset className='mb-4'>
+          <Label htmlFor='notes'>Additional Notes</Label>
+          <Textarea name='notes' id='notes' rows={4} />
+        </Fieldset>
+      </form>
+    </>
+  );
+};
+
+export default BookingForm;
