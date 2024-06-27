dd72be6d9c8e70cf3668072fec697caa1f2f82ef
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-27 16:56:56 +0100

    refactor: Update booking modal to handle slot objects instead of strings

--- components/compositions/common/selected-time-slots-list.tsx
+++ components/compositions/common/selected-time-slots-list.tsx
@@ -3,56 +3,68 @@ import { XMarkIcon } from '@heroicons/react/20/solid';
 import classNames from 'classnames';
 import { sortTimeSlots } from '@/lib/utils';
 import { periodColors } from '@/lib/constants';
+import { Sunrise, Sunset } from 'lucide-react';
 
 interface SelectedTimeSlotsListProps {
-  selectedTimeSlots: string[];
-  setSelectedTimeSlots?: (slots: (prev: string[]) => string[]) => void;
+  selectedTimeSlots: {
+    id: number;
+    time: string;
+    date: string;
+    period: string;
+    price?: number;
+  }[];
+  setSelectedTimeSlots: (value: any) => void;
 }
 
 const SelectedTimeSlotsList: React.FC<SelectedTimeSlotsListProps> = ({
   selectedTimeSlots,
   setSelectedTimeSlots,
 }) => {
-  interface TimeSlot {
-    startTime: string;
-    endTime: string;
-    date: string;
-    period: string;
-  }
-
-  const parseTimeSlot = (slot: string): TimeSlot => {
-    const [time, date, period] = slot.split('|||');
-    const [startTime, endTime] = time.split(' - ');
-    return { startTime, endTime, date, period };
+  const periodIcons: { [key: string]: JSX.Element } = {
+    morning: <Sunrise />,
+    afternoon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='currentColor'
+        className='size-6'
+      >
+        <path d='M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z' />
+      </svg>
+    ),
+    evening: <Sunset />,
+    night: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='currentColor'
+        className='size-6'
+      >
+        <path
+          fillRule='evenodd'
+          d='M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z'
+          clipRule='evenodd'
+        />
+      </svg>
+    ),
   };
 
-  const formatTimeSlot = (slot: TimeSlot): string => {
-    return `${slot.startTime} - ${slot.endTime}|||${slot.date}|||${slot.period}`;
-  };
-
-  const mergeTimeSlots = (slots: string[]): string[] => {
-    if (slots.length === 0) return [];
-
-    const parsedSlots = slots.map(parseTimeSlot);
-
-    parsedSlots.sort((a, b) => {
-      const dateA = new Date(a.date).getTime();
-      const dateB = new Date(b.date).getTime();
-      if (dateA !== dateB) return dateA - dateB;
-
-      const startTimeA = new Date(`1970/01/01 ${a.startTime}`).getTime();
-      const startTimeB = new Date(`1970/01/01 ${b.startTime}`).getTime();
-      return startTimeA - startTimeB;
-    });
-
-    const mergedSlots: TimeSlot[] = [];
-    let currentSlot = parsedSlots[0];
-
-    for (let i = 1; i < parsedSlots.length; i++) {
-      const nextSlot = parsedSlots[i];
-
+  const mergeTimeSlots = (
+    slots: {
+      id: number;
+      time: string;
+      date: string;
+      period: string;
+      price?: number;
+      startTime?: number;
+      endTime?: number;
+    }[]
+  ) => {
+    const mergedSlots = [];
+    let currentSlot = slots[0];
+    for (let i = 1; i < slots.length; i++) {
+      const nextSlot = slots[i];
       if (
-        currentSlot.endTime === nextSlot.startTime &&
         currentSlot.date === nextSlot.date &&
         currentSlot.period === nextSlot.period
       ) {
@@ -62,11 +74,11 @@ const SelectedTimeSlotsList: React.FC<SelectedTimeSlotsListProps> = ({
         currentSlot = nextSlot;
       }
     }
-
     mergedSlots.push(currentSlot);
-
-    return mergedSlots.map(formatTimeSlot);
+    return mergedSlots;
   };
+
+  sortTimeSlots(selectedTimeSlots);
 
   return (
     <>
@@ -77,40 +89,52 @@ const SelectedTimeSlotsList: React.FC<SelectedTimeSlotsListProps> = ({
       </h4>
       {selectedTimeSlots.length > 0 && (
         <ul role='list' className='mt-3 grid grid-cols-1 gap-5'>
-          {mergeTimeSlots(sortTimeSlots(selectedTimeSlots)).map((slot) => (
-            <li key={slot} className='col-span-1 flex rounded-md shadow-sm'>
+          {selectedTimeSlots.map((slot) => (
+            <li
+              key={slot.id}
+              className='col-span-1 flex rounded-md shadow-sm text-left'
+            >
               {periodColors && (
                 <div
                   className={classNames(
-                    periodColors[slot.split('|||')[2]],
+                    slot.period === 'morning' && 'bg-blue-200',
+                    slot.period === 'afternoon' && 'bg-yellow-400',
+                    slot.period === 'evening' && 'bg-orange-400',
+                    slot.period === 'night' && 'bg-blue-900 text-white',
                     'flex flex-shrink-0 items-center justify-center rounded-l-md text-sm text-gray-900 font-semibold px-4'
                   )}
                 >
-                  {slot.split('|||')[2].charAt(0).toUpperCase() +
-                    slot.split('|||')[2].slice(1)}
+                  {periodIcons[slot.period]}
                 </div>
               )}
               <div className='flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white'>
                 <div className='flex-1 truncate px-2 py-1 text-xs'>
                   <p className='font-medium text-gray-900 hover:text-gray-600'>
-                    {slot.split('|||')[0]}
+                    {slot.time}
                   </p>
-                  <p className='text-gray-500'>{slot.split('|||')[1]}</p>
+                  <p className='text-gray-500'>
+                    {new Date(slot.date).toLocaleDateString('en-US', {
+                      weekday: 'long',
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    })}
+                  </p>
                 </div>
-                {/* <div className='flex-shrink-0 pr-2'>
-                    <button
-                      type='button'
-                      onClick={() =>
-                        setSelectedTimeSlots((prev: string[]) =>
-                          prev.filter((time: string) => time !== slot)
-                        )
-                      }
-                      className='inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-red-500 hover:text-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-1'
-                    >
-                      <span className='sr-only'>Remove</span>
-                      <XMarkIcon className='h-5 w-5' aria-hidden='true' />
-                    </button>
-                  </div> */}
+                <div className='flex-shrink-0 pr-2'>
+                  <button
+                    type='button'
+                    onClick={() =>
+                      setSelectedTimeSlots((prev: any) =>
+                        prev.filter((s: any) => s.id !== slot.id)
+                      )
+                    }
+                    className='inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-red-500 hover:text-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-1'
+                  >
+                    <span className='sr-only'>Remove</span>
+                    <XMarkIcon className='h-5 w-5' aria-hidden='true' />
+                  </button>
+                </div>
               </div>
             </li>
           ))}
