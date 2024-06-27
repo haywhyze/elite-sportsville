c6a5292eaf3fa713056a7978006b6ff9a8c9263e
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-27 16:56:08 +0100

    refactor: Update period colors and add Tab interface in constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,21 @@
+import { Sunrise } from 'lucide-react';
+import { ReactNode } from "react";
+
 export const periodColors: { [key: string]: string } = {
-  morning: 'bg-yellow-400',
-  afternoon: 'bg-green-400',
-  evening: 'bg-blue-400',
-  night: 'bg-purple-400',
+  morning: 'bg-blue-800',
+  afternoon: 'bg-yellow-400',
+  evening: 'bg-orange-400',
+  night: 'bg-blue-900',
 };
+
+export interface Tab {
+  name: string;
+  id: string;
+}
+
+export const tabs: Tab[] = [
+  { name: 'Morning', id: 'morning' },
+  { name: 'Afternoon', id: 'afternoon' },
+  { name: 'Evening', id: 'evening' },
+  { name: 'Night', id: 'night' },
+];
