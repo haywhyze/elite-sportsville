f3081a8244639e357f05278f63cb042b4d7aed99
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-27 16:56:20 +0100

    refactor: Update parseDateTime function to handle slot objects instead of strings

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,8 +7,16 @@ export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
   }`
 }
 
-const parseDateTime = (slot: string) => {
-  const [time, date] = slot.split('|||')
+const parseDateTime = (slot: {
+  id: number;
+  time: string;
+  date: string;
+  period: string;
+  price?: number;
+  startTime?: number;
+  endTime?: number;
+}) => {
+  const { time, date } = slot;
   // make sure to return a Date object with the time and date
   const startTime = time.split(' - ')[0];
   // get the hour from the start time and convert to 24hr format
@@ -16,11 +24,56 @@ const parseDateTime = (slot: string) => {
   if (startTime.includes('pm')) {
     startHour = startHour === 12 ? startHour : startHour + 12;
   }
-  return new Date(`${date} ${startHour}:00:00`);
+  return new Date(`${date.split('T')[0]} ${startHour}:00:00`);
 };
 
-export const sortTimeSlots = (slots: string[]) => {
+export const sortTimeSlots = (slots: {
+  id: number;
+  time: string;
+  date: string;
+  period: string;
+  price?: number;
+  startTime?: number;
+  endTime?: number;
+}[]) => {
+  console.log({
+    parseDateTime: slots.map(parseDateTime),
+  })
   return slots.sort((a, b) => {
     return parseDateTime(a).getTime() - parseDateTime(b).getTime();
   });
 };
+
+function formatTime(hour: number) {
+  const period = hour < 12 ? "am" : "pm";
+  const adjustedHour = hour % 12 === 0 ? 12 : hour % 12;
+  return `${adjustedHour}${period}`;
+}
+
+function getPeriod(hour: number) {
+  if (hour >= 7 && hour < 12) return 'morning';
+  if (hour >= 12 && hour < 16) return 'afternoon';
+  if (hour >= 16 && hour < 19) return 'evening';
+  return 'night';
+}
+
+export function generateTimeSlotsForDate(date: Date) {
+  const startTime = 7; // Start time in hours (7 AM)
+  const endTime = 22; // End time in hours (10 PM)
+  const timeSlots = [];
+  for (let hour = startTime; hour < endTime; hour++) {
+    const startTimeString = formatTime(hour);
+    const endTimeString = formatTime(hour + 1);
+    const period = getPeriod(hour);
+
+    timeSlots.push({
+      id: `time-slot-${date.toDateString()}-${startTimeString} - ${endTimeString}-${period}`,
+      time: `${startTimeString} - ${endTimeString}`,
+      date: new Date(date).toISOString(),
+      period: period,
+      price: 5000
+    });
+  }
+
+  return timeSlots;
+}
