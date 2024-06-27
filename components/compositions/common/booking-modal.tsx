3e236a04adade496cdb943b589601866097d7766
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-06-27 16:56:56 +0100

    refactor: Update booking modal to handle slot objects instead of strings

--- components/compositions/common/booking-modal.tsx
+++ components/compositions/common/booking-modal.tsx
@@ -16,70 +16,49 @@ import BookingForm from './booking-form';
 import { X } from 'lucide-react';
 import PaymentMothod from './payment-method';
 import PaymentInfo from './payment-info';
+import { generateTimeSlotsForDate } from '@/lib/utils';
+import { Tab, tabs } from '@/lib/constants';
 
 interface BookingModalProps {
   isOpen: boolean;
   setIsOpen: (value: boolean) => void;
 }
 
-interface Tab {
-  name: string;
-  id: string;
-  current: boolean;
-}
-
-const tabs: Tab[] = [
-  { name: 'Morning', id: 'morning', current: true },
-  { name: 'Afternoon', id: 'afternoon', current: false },
-  { name: 'Evening', id: 'evening', current: false },
-  { name: 'Night', id: 'night', current: false },
-];
-
 const BookingModal: React.FC<BookingModalProps> = ({ isOpen, setIsOpen }) => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [selectedTab, setSelectedTab] = useState<Tab>(tabs[0]);
-  const [selectedTimeSlots, setSelectedTimeSlots] = useState<string[]>([]);
+  const [selectedTimeSlots, setSelectedTimeSlots] = useState<{
+    id: number;
+    time: string;
+    date: string;
+    period: string;
+    price?: number;
+  }[]>([]);
   const [bookingSteps, setBookingSteps] = useState<number>(1);
   const [paymentMethod, setPaymentMethod] = useState<string>('');
 
-  const timeSlots = Array.from({ length: 15 }, (_, index) => {
-    const hour = index + 7;
-    const nextHour = hour + 1;
-    return `${hour % 12 || 12}${hour < 12 ? 'am' : 'pm'} - ${
-      nextHour % 12 || 12
-    }${nextHour < 12 ? 'am' : 'pm'}`;
-  });
+  const timeSlots = generateTimeSlotsForDate(selectedDate || new Date());
 
-  const timeSlotsIntoPeriods = {
-    morning: timeSlots.slice(0, 5),
-    afternoon: timeSlots.slice(5, 9),
-    evening: timeSlots.slice(9, 12),
-    night: timeSlots.slice(12),
-  };
+  const timeSlotsIntoPeriods = timeSlots.reduce((acc, slot) => {
+    if (!acc[slot.period]) {
+      acc[slot.period] = [];
+    }
+    acc[slot.period].push(slot);
+    return acc;
+  }, {} as Record<string, any>);
 
   const handleDateChange = (date: Date | null) => setSelectedDate(date);
 
-  const handleTimeSlotSelect = (slot: string) => {
-    if (selectedTimeSlots.includes(slot)) {
-      setSelectedTimeSlots((prev) => prev.filter((time) => time !== slot));
+  const onSelectTimeSlot = (slot: any) => {
+    if (selectedTimeSlots.map((t) => t.id).includes(slot.id)) {
+      setSelectedTimeSlots((prev) =>
+        prev.filter((time) => time.id !== slot.id)
+      );
     } else if (selectedTimeSlots.length < 5) {
       setSelectedTimeSlots((prev) => [...prev, slot]);
     } else {
       notifyErrorMessage('You can only book a maximum of 5 slots at a time');
     }
-  };
-
-  const handleTimeSlotSelectObject = (
-    time: string,
-    date: string,
-    period: string
-  ) => {
-    const slot = {
-      id: `${time}|||${date}|||${period}`,
-      time,
-      date,
-      period,
-    };
   };
 
   const notifyErrorMessage = (message: string) => {
@@ -132,50 +111,24 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, setIsOpen }) => {
                         selectedTab={selectedTab}
                         setSelectedTab={setSelectedTab}
                       />
-                      {Object.entries(timeSlotsIntoPeriods).map(
-                        ([period, slots]) => {
-                          if (period === selectedTab.id)
-                            return (
-                              <div key={period} className='mb-6 w-full'>
-                                <div className='flex flex-wrap gap-2 mt-6 justify-center items-center lg:justify-start'>
-                                  {slots.map((time) => (
-                                    <TimeSlot
-                                      key={time}
-                                      time={time}
-                                      price={5000}
-                                      isBooked={time === '9am - 10am'}
-                                      onSelect={() => {
-                                        handleTimeSlotSelect(
-                                          `${time}|||${selectedDate?.toLocaleDateString(
-                                            'en-GB',
-                                            {
-                                              year: 'numeric',
-                                              month: 'long',
-                                              day: 'numeric',
-                                              weekday: 'long',
-                                            }
-                                          )}|||${period}`
-                                        );
-                                      }}
-                                      selected={selectedTimeSlots.includes(
-                                        `${time}|||${selectedDate?.toLocaleDateString(
-                                          'en-GB',
-                                          {
-                                            year: 'numeric',
-                                            month: 'long',
-                                            day: 'numeric',
-                                            weekday: 'long',
-                                          }
-                                        )}|||${period}`
-                                      )}
-                                    />
-                                  ))}
-                                </div>
-                              </div>
-                            );
-                          return null;
-                        }
-                      )}
+                      <div className='flex flex-wrap gap-2 my-6 justify-center items-center sm:justify-start'>
+                        {timeSlotsIntoPeriods[selectedTab.id].map(
+                          (time: any) => (
+                            <TimeSlot
+                              key={time.id} // Add a unique key for each time slot
+                              time={time.time}
+                              price={time.price}
+                              isBooked={time.isBooked}
+                              onSelect={() => {
+                                onSelectTimeSlot(time);
+                              }}
+                              selected={selectedTimeSlots
+                                .map((t) => t.id)
+                                .includes(time.id)}
+                            />
+                          )
+                        )}
+                      </div>
                     </div>
                   )}
                 </div>
@@ -199,6 +152,7 @@ const BookingModal: React.FC<BookingModalProps> = ({ isOpen, setIsOpen }) => {
                   <BookingForm
                     selectedDate={selectedDate}
                     selectedTimeSlots={selectedTimeSlots}
+                    setSelectedTimeSlots={setSelectedTimeSlots}
                   />
                 </div>
                 <div className='sm:w-1/2'>
