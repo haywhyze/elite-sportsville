42b9817a40adf93ba9b4b41acbf96bc9783d0c56
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-07-04 20:39:39 +0100

    refactor: Update firebase.js to check if analytics is supported before initializing it

--- components/compositions/header.tsx
+++ components/compositions/header.tsx
@@ -79,7 +79,7 @@ export default function Header() {
           <div className='flex items-center gap-x-6'>
             <a href='#' className='-m-1.5 p-1.5'>
               <span className='sr-only'>Your Company</span>
-              <img className='h-8 w-auto' src='logo.png' alt='' />
+              {/* <img className='h-8 w-auto' src='logo.png' alt='' /> */}
             </a>
             <a
               href='#'
