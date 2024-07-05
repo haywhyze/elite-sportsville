453b0f1d75279ea27a715f760c1373d962990837
Yusuf Abdulkarim <haywhyze@gmail.com>
2024-07-05 10:30:12 +0100

    refactor: Update carousel autoplay delay to 5000ms

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -48,7 +48,7 @@ const Carousel = () => {
       spaceBetween={30}
       centeredSlides={true}
       autoplay={{
-        delay: 30000,
+        delay: 5000,
         disableOnInteraction: false,
       }}
       pagination={{
